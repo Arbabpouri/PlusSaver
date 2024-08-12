f04f2a86fd71e5b0422b70d0a38f1fd723389386
MohammadMahdi Arbabpouri <sardarcyberym@gmail.com>
2024-08-12 11:45:14 +0330

    delete enums.py and make methods in Regexs class

---
+++ modules/regexs.py
@@ -0,0 +1,25 @@
+class Regexs:
+
+
+    def __init__(self, url: str) -> None:
+        self.url = url
+    
+    @property
+    def instagram(self) -> bool:
+        pass
+
+    @property
+    def youtube(self) -> bool:
+        pass
+
+    @property
+    def soundcloud(self) -> bool:
+        pass
+
+    @property
+    def spotify(self) -> bool:
+        pass
+
+    @property
+    def tiktok(self) -> bool:
+        pass
