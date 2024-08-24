284b7acab2395b01b42cfc0b03022decc6a4792e
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-08-24 13:50:39 +0330

    update

--- config/config.py
+++ config/config.py
@@ -2,20 +2,20 @@
 
 class BotConfig:
     SESSION_NAME = "PlusSaverSession"
-    API_ID = 123
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 10603708
+    API_HASH = "f17d354888d912173f1599ca3133e04e"
+    BOT_TOKEN = "7471723600:AAHuEcXW7bBg_ryNbgxG5KcovE0-ptfZyr4"
     BOT_USERNAME = "PlusSaverBot"  # Without @, example Folabetbot
     DEFULT_ADMINS_USER_ID = {2056493966, }
     CREATOR_USER_ID = 2056493966
     SUPPORT_USERNAME = "Jarrare" # without @, example Jarrae
     REFERRAL_IMAGE_ADDRESS = r"./images/referral.png"
     DEFULT_SUPPORT_CHANNEL_URL = "t.me/MohammadMahdiArbabpouri" # url form, not username
-    DONATE = "doante url"
+    DONATE = ""
     DEFULT_ENTRY_PRIZE = 5000
     DEFULT_REFERRAL_BONUS = 1000
     TEXT_LONG = 3000
-    MEDIAS_CHANNEL_ID = 123
+    MEDIAS_CHANNEL_ID = 2232637889
     DEFULT_RULES_TEXT = (
         "### Download Bot Usage Rules 📥\n\n"
 
@@ -79,3 +79,83 @@ class BotConfig:
 
         "Thank you for using our service! 🙌❤️"
     )
+
+# class BotConfig:
+#     SESSION_NAME = "PlusSaverSession"
+#     API_ID = 123
+#     API_HASH = ""
+#     BOT_TOKEN = ""
+#     BOT_USERNAME = "PlusSaverBot"  # Without @, example Folabetbot
+#     DEFULT_ADMINS_USER_ID = {2056493966, }
+#     CREATOR_USER_ID = 2056493966
+#     SUPPORT_USERNAME = "Jarrare" # without @, example Jarrae
+#     REFERRAL_IMAGE_ADDRESS = r"./images/referral.png"
+#     DEFULT_SUPPORT_CHANNEL_URL = "t.me/MohammadMahdiArbabpouri" # url form, not username
+#     DONATE = "doante url"
+#     DEFULT_ENTRY_PRIZE = 5000
+#     DEFULT_REFERRAL_BONUS = 1000
+#     TEXT_LONG = 3000
+#     MEDIAS_CHANNEL_ID = 123
+#     DEFULT_RULES_TEXT = (
+#         "### Download Bot Usage Rules 📥\n\n"
+
+#         "Hello dear users! 👋\n\n"
+
+#         "For optimal and safe use of the download bot, please pay attention to the following rules:\n\n"
+
+#         "1. Legal Use** ⚖️\n"
+#         "Please only download content that is authorized and has appropriate copyright rights. This bot is designed for educational and personal use.\n\n"
+
+#         "2. **No Misuse** 🚫\n"
+#         "Do not use the bot for illegal purposes or any other form of abuse. Any violations may result in the suspension of your user account.\n\n"
+
+#         "3. **No Disruption** ⚠️\n"
+#         "Please do not attempt to disrupt the bot's functionality in any way. Such actions not only harm other users but may also have legal consequences.\n\n"
+
+#         "4. **Respect Others** 🤝\n"
+#         "Respect the rights of other users and refrain from uploading offensive or inappropriate content.\n\n"
+
+#         "5. **Report Issues** 🛠\n"
+#         "If you encounter any problems or errors in the bot's functionality, please let us know so we can address them promptly.\n\n"
+
+#         "By adhering to these rules, we can create a better and safer experience for everyone. 🙌\n\n"
+
+#         "Thank you for your cooperation! 💖"
+#     )
+#     DEFULT_HELP_TEXT = (
+
+#         "### Download Bot Usage Guide 🎉\n\n"
+
+#         "Hello friends! 👋\n\n"
+
+#         "With our download bot, you can easily download videos and music from YouTube, Instagram, SoundCloud, and other platforms by simply sending a link. 📥✨\n\n"
+
+#         "#### Steps to Use the Bot:\n\n"
+
+#         "1. **Send the Link** 🔗\n"
+#         "Just send the link of the YouTube video, Instagram post, or SoundCloud track to the bot. The bot will quickly start processing and downloading the content.\n\n"
+
+#         "2. **Wait a Moment** ⏳\n"
+#         "After sending the link, please wait a moment for the bot to prepare the requested file. This process is usually quick!\n\n"
+
+#         "3. **Receive the File** 📤\n"
+#         "Once the download is complete, the bot will send you the file. Now you can enjoy the downloaded content! 🎶🎥\n\n"
+
+#         "#### Adding the Bot to a Telegram Group:\n\n"
+
+#         "1. **Search for the Bot** 🔍\n"
+#         "In Telegram, search for the bot's username.\n\n"
+
+#         "2. **Add to Group** ➕\n"
+#         "Click on the bot's name and then select the \"Add to Group\" option.\n\n"
+
+#         "3. **Set Permissions** ⚙️\n"
+#         "Make sure the bot has the necessary permissions to send messages in the group.\n\n"
+
+#         "4. **Using the Bot in the Group** 👥\n"
+#         "Now any group member can send links to the bot and take advantage of its services!\n\n"
+
+#         "By following these steps, you can easily utilize the features of the download bot. If you have any questions, feel free to reach out! 💬\n\n"
+
+#         "Thank you for using our service! 🙌❤️"
+#     )
