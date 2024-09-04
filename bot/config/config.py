652e29387c8458efe194c9afdab834b82de9e5e2
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-09-04 16:30:32 +0330

    update

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,19 +1,20 @@
-# coded with Love by MohammadMahdi Arbabpouri
+# # coded with Love by MohammadMahdi Arbabpouri
 
 
 class BotConfig:
     SESSION_NAME = "PlusSaverSession"
-    API_ID = 123
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 10603708
+    API_HASH = "f17d354888d912173f1599ca3133e04e"
+    # BOT_TOKEN = "7471723600:AAHuEcXW7bBg_ryNbgxG5KcovE0-ptfZyr4"
+    BOT_TOKEN = "6657555351:AAHPSb3X6YVjOA7dc3v4-8CRkxpg3x_dAKc"
     BOT_USERNAME = "PlusSaverBot"  # Without @, example Folabetbot
     DEFULT_ADMINS_USER_ID = {2056493966, }
     CREATOR_USER_ID = 2056493966
     SUPPORT_USERNAME = "Jarrare" # without @, example Jarrae
     DEFULT_SUPPORT_CHANNEL_URL = "t.me/MohammadMahdiArbabpouri" # url form, not username
-    DONATE = "doante url"
+    DONATE = ""
     TEXT_LONG = 3000
-    MEDIAS_CHANNEL_ID = 123
+    MEDIAS_CHANNEL_ID = 2232637889
     DEFULT_RULES_TEXT = (
         "### Download Bot Usage Rules 📥\n\n"
 
@@ -24,16 +25,16 @@ class BotConfig:
         "1. Legal Use** ⚖️\n"
         "Please only download content that is authorized and has appropriate copyright rights. This bot is designed for educational and personal use.\n\n"
 
-        "2. **No Misuse** 🚫\n"
+        "2. No Misuse 🚫\n"
         "Do not use the bot for illegal purposes or any other form of abuse. Any violations may result in the suspension of your user account.\n\n"
 
-        "3. **No Disruption** ⚠️\n"
+        "3. No Disruption ⚠️\n"
         "Please do not attempt to disrupt the bot's functionality in any way. Such actions not only harm other users but may also have legal consequences.\n\n"
 
-        "4. **Respect Others** 🤝\n"
+        "4. Respect Others 🤝\n"
         "Respect the rights of other users and refrain from uploading offensive or inappropriate content.\n\n"
 
-        "5. **Report Issues** 🛠\n"
+        "5. Report Issues 🛠\n"
         "If you encounter any problems or errors in the bot's functionality, please let us know so we can address them promptly.\n\n"
 
         "By adhering to these rules, we can create a better and safer experience for everyone. 🙌\n\n"
@@ -50,27 +51,27 @@ class BotConfig:
 
         "#### Steps to Use the Bot:\n\n"
 
-        "1. **Send the Link** 🔗\n"
+        "1. Send the Link 🔗\n"
         "Just send the link of the YouTube video, Instagram post, or SoundCloud track to the bot. The bot will quickly start processing and downloading the content.\n\n"
 
-        "2. **Wait a Moment** ⏳\n"
+        "2. Wait a Moment ⏳\n"
         "After sending the link, please wait a moment for the bot to prepare the requested file. This process is usually quick!\n\n"
 
-        "3. **Receive the File** 📤\n"
+        "3. Receive the File 📤\n"
         "Once the download is complete, the bot will send you the file. Now you can enjoy the downloaded content! 🎶🎥\n\n"
 
         "#### Adding the Bot to a Telegram Group:\n\n"
 
-        "1. **Search for the Bot** 🔍\n"
+        "1. Search for the Bot 🔍\n"
         "In Telegram, search for the bot's username.\n\n"
 
-        "2. **Add to Group** ➕\n"
+        "2. Add to Group ➕\n"
         "Click on the bot's name and then select the \"Add to Group\" option.\n\n"
 
-        "3. **Set Permissions** ⚙️\n"
+        "3. Set Permissions ⚙️\n"
         "Make sure the bot has the necessary permissions to send messages in the group.\n\n"
 
-        "4. **Using the Bot in the Group** 👥\n"
+        "4. Using the Bot in the Group 👥\n"
         "Now any group member can send links to the bot and take advantage of its services!\n\n"
 
         "By following these steps, you can easily utilize the features of the download bot. If you have any questions, feel free to reach out! 💬\n\n"
