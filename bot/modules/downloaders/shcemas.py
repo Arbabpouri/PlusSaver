fa2d11b4eb77966520dd4e184061fcb49aa2bd0f
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-09-08 18:56:21 +0330

    clean

--- bot/modules/downloaders/shcemas.py
+++ bot/modules/downloaders/shcemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class MediaDownloaded(BaseModel):
-    MEDIA: str | bytes |None = None
+    MEDIA: str | bytes | None = None
     TITLE: str | None = None
     CAPTION: str | None = None
     RESULT: bool | None = None
