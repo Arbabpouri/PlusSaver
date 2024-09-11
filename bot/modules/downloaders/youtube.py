751011a75a0091f140475c99f89439d058767464
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-09-11 08:23:02 +0330

    fix youtube

--- bot/modules/downloaders/youtube.py
+++ bot/modules/downloaders/youtube.py
@@ -7,28 +7,22 @@ from .base import BaseDownloader
 
 
 class DownloadResponseMedia(BaseModel):
-    formatId: int
-    label: str
-    type: str
-    ext: str
-    width: int | None = None
-    height: int | None = None
     url: str
-    bitrate: int
-    fps: int | None = None
-    audioQuality: str | None = None
-    audioSampleRate: str | None = None
-    mimeType: str
-    duration: int
-    
+    quality: str
+    is_audio: bool | None = None
+    type: str
+    extension: str
     
 class DownloadResponseMedias(BaseModel):
-    defaultFormatId: int
-    duration: str
-    formats: list[DownloadResponseMedia]
-    thumbnailUrl: str
+    url: str
+    source: str
+    author: str
     title: str
-
+    thumbnail: str
+    duration: int
+    medias: list[DownloadResponseMedia] | None = None
+    type: str
+    error: bool
 
 class Youtube(BaseDownloader):
     
@@ -40,24 +34,50 @@ class Youtube(BaseDownloader):
         }
         
         self.__headers = {
-            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36"
+            # "content-type": "application/json",
+            # # "Content-Length": "<calculated when request is sent>",
+            # # "Host": "<calculated when request is sent>",
+            # "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
+            # "accept": "*/*",
+            # "accept-language": "en-US,en;q=0.9",
+            # "accept-encoding": "gzip, deflate, br",
+            # "connection": "keep-alive",
+            
+            # "accept": "*/*",
+            # "accept-encoding": "gzip, deflate, br, zstd",
+            # "accept-language": "en-US,en;q=0.9",
+            # # content-length: 53
+            # "content-type": "application/json",
+            # "origin": "https://submagic-free-tools.fly.dev",
+            # "priority": "u=1, i",
+            # "referer": "https://submagic-free-tools.fly.dev/youtube-downloader",
+            # "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+            # "sec-ch-ua-mobile": "?1",
+            # "sec-ch-ua-platform": '"Android"',
+            # "sec-fetch-dest": "empty",
+            # "sec-fetch-mode": "cors",
+            # "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
         }
                 
     async def download_video(self) -> MediaDownloaded:
         
-        async with aiohttp.ClientSession() as session:
-                
-            async with session.post(url=self.__api_url, data=self.__data, headers=self.__headers) as response:
-            
-                if response.status == 200:
+        try:
                     
-                    try:
-                        
+            async with aiohttp.ClientSession() as session:
+                    
+                async with session.post(url=self.__api_url, data=self.__data, headers=self.__headers) as response:
+                
+                    if response.status == 200:
+                                                    
                         data = await response.json()                        
                         medias = DownloadResponseMedias(**data)
                         
-                        for media in medias.formats:
-                            if media.type == 'video_with_audio' and  media.duration < 1000000:
+                        if medias.error:
+                            return MediaDownloaded(RESULT=False)
+                        
+                        for media in medias.medias:
+                            if media.type == 'video' and media.is_audio:
                                 title = medias.title
                                 url = media.url
                                 del medias
@@ -70,13 +90,17 @@ class Youtube(BaseDownloader):
                                             file.write(await video.read())
                                         del video
                                         return MediaDownloaded(MEDIA=file_path, TITLE=title, RESULT=True)
+                                
+                                break
                             
                             return MediaDownloaded(RESULT=None)
                                         
-                    except Exception as e:
-                        print("Error is Youtube download media :", e)
+        except Exception as e:
+            print("Error is Youtube download media :", e)
         
         return MediaDownloaded(RESULT=False)
         
 
         
+
+
