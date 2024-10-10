5849d91553c2199d10a5f9e68ec04f55447834d6
arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-10-10 20:56:47 +0330

    add MediasDownloaded class

--- bot/modules/downloaders/shcemas.py
+++ bot/modules/downloaders/shcemas.py
@@ -2,8 +2,11 @@ from pydantic import BaseModel
 
 
 class MediaDownloaded(BaseModel):
-    MEDIA: str | bytes | None = None
+    MEDIA: str | bytes |None = None
     TITLE: str | None = None
     CAPTION: str | None = None
     RESULT: bool | None = None
-    
+
+
+class MediasDownloaded(BaseModel):
+    MEDIAS: list[MediaDownloaded] | None = None
